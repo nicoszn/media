869ff556874b361020bf6a5b1c24e410c2474ae3
nicoszn <141965889+nicoszn@users.noreply.github.com>
2026-06-18 04:01:56 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
-  async rewrites() {
+  serverExternalPackages: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+  async headers() {
     return [
       {
-        source: '/api/video/info',
-        destination: 'https://web-production-a9b29.up.railway.app/info',
+        source: "/(.*)",
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+          { key: "Cross-Origin-Resource-Policy", value: "cross-origin" },
+        ],
       },
     ];
   },
-  turbopack: {},
 };
 
 export default nextConfig;
